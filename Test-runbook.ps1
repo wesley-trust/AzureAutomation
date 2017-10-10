ab8d640263faac2967b233fea69282a4c3da1d0a
Wesley Trust <wesley.trust@rosslynanalytics.com>
2017-10-10 17:02:41 +0100

    Update to include resource group name, add added additional error info for failure to get VM info.

--- Test-runbook.ps1
+++ Test-runbook.ps1
@@ -3,8 +3,7 @@
         An example runbook which gets all the ARM VMs using the Run As Account (Service Principal)
 
     .NOTES
-        Based on the Azure Automation Team script
-       
+        Based on the Azure Automation Team script   
 #>
 
 $connectionName = "AzureRunAsConnection"
@@ -32,11 +31,16 @@ catch {
 }
 
 #Get VM with specific name that is deallocated
-$VMs = Get-AzureRMVM -Status | Where-Object {$_.Name -eq "WES-LAB" -and $_.PowerState -like "*deallocated*"}
+try {
+    $VMs = Get-AzureRMVM -ResourceGroupName "TESTLAB223916554000"  -Status | Where-Object {$_.Name -eq "WES-LAB" -and $_.PowerState -like "*deallocated*"}
+}
+Catch {
+    Write-Error -Message $_.Exception -ErrorAction Stop
+}
 
 #If no VMs are returned throw exception
 if (!$VMs){
-    throw "No VMs to be started"
+    throw "No VMs to be started."
 }
 
 #Else start VMs
