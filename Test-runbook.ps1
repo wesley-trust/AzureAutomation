04342a37d7269a668bec40b492fe6ea548b3c8b1
Wesley Trust <wesley.trust@rosslynanalytics.com>
2017-10-10 20:07:52 +0100

    Updated to function, with new logic for checking deallocated status.

--- Test-runbook.ps1
+++ Test-runbook.ps1
@@ -6,60 +6,69 @@
         Based on the Azure Automation Team script   
 #>
 
-# Parameters
-Param(
-    # Resource group
-    [Parameter(
-        Mandatory=$true,
-        HelpMessage="Enter the resource group that all VMs belong to"
-    )]
-    [string]
-    $ResourceGroupName,
-    
-    # VM Names
-    [Parameter(
-        Mandatory=$true,
-        HelpMessage="Enter VM names, comma separated"
-    )]
-    [string[]]
-    $VMNames
-)
-
-$connectionName = "AzureRunAsConnection"
-try
-{
-    # Get the connection "AzureRunAsConnection "
-    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
-
-    "Logging in to Azure..."
-    Add-AzureRmAccount `
-        -ServicePrincipal `
-        -TenantId $servicePrincipalConnection.TenantId `
-        -ApplicationId $servicePrincipalConnection.ApplicationId `
-        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
-}
-catch {
-    if (!$servicePrincipalConnection)
-    {
-        $ErrorMessage = "Connection $connectionName not found."
-        throw $ErrorMessage
-    } else{
-        Write-Error -Message $_.Exception
-        throw $_.Exception
-    }
-}
-
-#Get VM with specific name that is deallocated
-try {
-    foreach ($VMName in $VMNames){
-        #Get VM Object
-        $VMObject = Get-AzureRMVM -ResourceGroupName $ResourceGroupName -Status -Name $VMName | Where-Object PowerState -like "*deallocated*"
+function Start-VMs (){
+    # Parameters
+    Param(
+        # Resource group
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="Enter the resource group that all VMs belong to"
+        )]
+        [string]
+        $ResourceGroupName,
         
-        #Start VM
-        $VMObject | Start-AzureRmVM
+        # VM Names
+        [Parameter(
+            Mandatory=$true,
+            HelpMessage="Enter VM names, comma separated"
+        )]
+        [string[]]
+        $VMNames
+    )
+    Begin {
+        $connectionName = "AzureRunAsConnection"
+        try
+        {
+            # Get the connection "AzureRunAsConnection "
+            $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+    
+            "Logging in to Azure..."
+            Add-AzureRmAccount `
+                -ServicePrincipal `
+                -TenantId $servicePrincipalConnection.TenantId `
+                -ApplicationId $servicePrincipalConnection.ApplicationId `
+                -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+        }
+        catch {
+            if (!$servicePrincipalConnection)
+            {
+                $ErrorMessage = "Connection $connectionName not found."
+                throw $ErrorMessage
+            } else{
+                Write-Error -Message $_.Exception
+                throw $_.Exception
+            }
+        }
     }
-}
-Catch {
-    Write-Error -Message $_.Exception
-    throw $_.Exception
+
+    Process {
+        #Get VM with specific name that is deallocated
+        try {
+            foreach ($VMName in $VMNames){
+                #Get VM Object
+                $VMObject = Get-AzureRMVM -ResourceGroupName $ResourceGroupName -Status -Name $VMName | Where-Object {($_.Statuses)[1].DisplayStatus -like "*deallocated*"}
+                
+                #Start VM
+                $VMObject | Start-AzureRmVM
+            }
+        }
+        Catch {
+            Write-Error -Message $_.Exception
+            throw $_.Exception
+        }
+    }
+
+    End {
+
+    }
 }
